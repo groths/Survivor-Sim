7641936f9370f52725db4d8aad63b31e78d79d9e
Rachel Groth <Rachel@Rachels-MBP-2.home>
2017-05-25 18:00:22 -0400

    added notes to main class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
     private static final int TOTAL_CONTESTANTS = 18;
     
-    private static final int FINALE_CONTESTANTS = 18;
+    private static final int FINALE_CONTESTANTS = 3;
     
     private static int day = 0;
     
@@ -37,9 +37,29 @@ public class Main {
         System.out.println("Ending main method");
     }
         
+    /**
+     * NOW: 
+     * This is a lot but...
+     * Create reward challenge method
+     * Create immunity challenge method
+     * Create tribal council method
+     * (We can figure out a system to have these each every few days)
+     * 
+     * And also just like a general neutral day thing where you get the option
+     * to relax/sleep, look for immunity idol, be social, etc.
+     * So maybe if the day is a certain multiple we can have the special things
+     * like challenges, tribal council, etc. but otherwise not
+     * 
+     * and maybe eventually we can add a random number generator where if it
+     * gets a certain number certain cool/random things happen (like you see an animal or something)
+     * 
+     * and then we can also incorporate when it gets to a certain day special things happen
+     * (such as merge, loved ones visit, auction challenge, etc. any fun things we want to add!)
+     */
     private static void runDay()
     {
         
+         
     }
     
 }
