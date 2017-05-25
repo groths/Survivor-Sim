1d4c0d6848f41807bc6b871eaa1383568ca40201
matthew-groth <mgroth49@gmail.com>
2017-05-25 16:56:52 -0400

    made runnable from netbeans

---
+++ src/main/java/main/Main.java
@@ -0,0 +1,16 @@
+package main;
+
+/**
+ *
+ * @author Rachel, Matt
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("hi matt");
+    }
+    
+}
