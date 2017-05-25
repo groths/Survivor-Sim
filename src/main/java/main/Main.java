a8e0178a9923381304597afa3481d6f3d60aa8e8
matthew-groth <mgroth49@gmail.com>
2017-05-25 17:41:37 -0400

    worked on main method/class

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,16 +1,45 @@
 package main;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Rachel, Matt
  */
 public class Main {
 
+    private static final int TOTAL_CONTESTANTS = 18;
+    
+    private static final int FINALE_CONTESTANTS = 18;
+    
+    private static int day = 0;
+    
+    private static ArrayList<Contestant> allContestants = new ArrayList<>(TOTAL_CONTESTANTS);
+    
+    private static ArrayList<Contestant> remainingContestants = new ArrayList<>(TOTAL_CONTESTANTS);
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hi matt");
+        
+        System.out.println("Starting Program");
+        System.out.println("Welcome to Surivivor-Sim!");
+        
+        
+        while (remainingContestants.size() > FINALE_CONTESTANTS)
+        {
+            runDay();
+            day++;
+        }
+        
+        
+        System.out.println("Ending main method");
+    }
+        
+    private static void runDay()
+    {
+        
     }
     
 }
